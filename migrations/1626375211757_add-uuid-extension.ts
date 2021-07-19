bd64c2d6a63d77e9b9991aba42ce4cf068f805a4
Nicolás Santángelo <santangelonicolas@gmail.com>
2021-07-19 19:28:51 -0300

    feat: revert uuids for now

--- migrations/1626375211757_add-uuid-extension.ts
+++
@@ -1,9 +0,0 @@
-import { MigrationBuilder } from 'node-pg-migrate'
-
-export const up = async (pgm: MigrationBuilder) => {
-  pgm.createExtension('uuid-ossp', { ifNotExists: true })
-}
-
-export const down = async (pgm: MigrationBuilder) => {
-  pgm.dropExtension('uuid-ossp')
-}
